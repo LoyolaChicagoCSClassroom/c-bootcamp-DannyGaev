89ffb99365619b1cfeabb14dfe6fb9d96cf609b1
Daniel Gaevskiy <119715495+DannyGaev@users.noreply.github.com>
2024-02-13 04:15:57 +0000

    Created token.c and the necessary classes within -- parseTokens determineswhat kind of token is being evaluated, and resolveToString helps main.c properly print token type values

---
+++ main/token.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "token.h"
+
+TOKEN parseTokens(char *token)
+{
+    
+    TOKEN returnToken;
+    if(*token=='+' || *token=='-' || *token=='*' || *token=='/')
+    {
+        returnToken.type_t = ARITH_OP;
+    }
+    else if(*token==':' || *token==';')
+    {
+        returnToken.type_t = SYMB;
+    }
+    else if(isdigit(*token)!=0)
+    {
+        returnToken.type_t = NUM;
+    }
+    else
+    {
+        returnToken.type_t = WORD;
+    }
+
+    returnToken.text = token;
+
+    return returnToken;
+}
+
+char *resolveToString(enum token_type_t type_t)
+{
+    if(type_t==WORD)
+    {
+        return "Word";
+    }
+    else if(type_t==NUM)
+    {
+        return "Number";
+    }
+    else if(type_t==SYMB)
+    {
+        return "Symbol";
+    }
+    else if(type_t==ARITH_OP)
+    {
+        return "Arithmetic Operation";
+    }
+}
+
