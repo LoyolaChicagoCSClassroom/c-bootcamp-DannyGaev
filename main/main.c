c909cfc3092a19f3de357df0fdc2ed5edad1119b
Daniel Gaevskiy <119715495+DannyGaev@users.noreply.github.com>
2024-02-13 04:14:55 +0000

    Created initial version of main.c; command-line input is used to evaluate the tokens

---
+++ main/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "token.c"
+
+int main(int argc, char *argv[])
+{
+    
+    char *token;
+
+    char *input = argv[1];
+
+    token = strtok(input," ");
+    
+    while(token != NULL) 
+    {
+        TOKEN returnToken = parseTokens(token);
+        printf("CLASS: %s, TEXT: %s\n",resolveToString(returnToken.type_t),returnToken.text);
+        token = strtok(NULL," ");
+    }
+    return EXIT_SUCCESS;
+}
