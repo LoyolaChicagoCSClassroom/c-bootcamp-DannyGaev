e629f9fba4741c2e5577fdeb1765c8a6d6e759f3
Daniel Gaevskiy <119715495+DannyGaev@users.noreply.github.com>
2024-02-13 04:21:36 +0000

    Removed unecessary #include imports

--- main/token.c
+++ main/token.c
@@ -1,29 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <ctype.h>
 #include "token.h"
 
 TOKEN parseTokens(char *token)
 {
-    
     TOKEN returnToken;
-    if(*token=='+' || *token=='-' || *token=='*' || *token=='/')
-    {
-        returnToken.type_t = ARITH_OP;
-    }
-    else if(*token==':' || *token==';')
-    {
-        returnToken.type_t = SYMB;
-    }
-    else if(isdigit(*token)!=0)
-    {
-        returnToken.type_t = NUM;
-    }
-    else
-    {
-        returnToken.type_t = WORD;
-    }
 
+    if(*token=='+' || *token=='-' || *token=='*' || *token=='/') returnToken.type_t = ARITH_OP;
+
+    else if(*token==':' || *token==';') returnToken.type_t = SYMB;
+    
+    else if(isdigit(*token)!=0) returnToken.type_t = NUM;
+    
+    else returnToken.type_t = WORD;
+    
     returnToken.text = token;
 
     return returnToken;
@@ -31,21 +20,12 @@ TOKEN parseTokens(char *token)
 
 char *resolveToString(enum token_type_t type_t)
 {
-    if(type_t==WORD)
-    {
-        return "Word";
-    }
-    else if(type_t==NUM)
-    {
-        return "Number";
-    }
-    else if(type_t==SYMB)
-    {
-        return "Symbol";
-    }
-    else if(type_t==ARITH_OP)
-    {
-        return "Arithmetic Operation";
-    }
+    if(type_t==WORD) return "Word";
+    
+    else if(type_t==NUM) return "Number";
+    
+    else if(type_t==SYMB) return "Symbol";
+    
+    else if(type_t==ARITH_OP) return "Arithmetic Operation";
 }
 
