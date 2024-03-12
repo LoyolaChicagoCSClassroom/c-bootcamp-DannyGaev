d5f8266e574535ede25a0c3f95a05ddd9874a2b3
OakTree-42 <DannyGaev@gmail.com>
2024-03-12 15:26:38 -0500

    Added edits to files

---
+++ std-queue/build/CMakeFiles/wordlist-demo.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for wordlist-demo.
+# This may be replaced when dependencies are built.
---
+++ std-queue/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/oaktree/Documents/GitHub/c-bootcamp-DannyGaev/std-queue
+# Build directory: /Users/oaktree/Documents/GitHub/c-bootcamp-DannyGaev/std-queue/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/Users/oaktree/Documents/GitHub/c-bootcamp-DannyGaev/std-queue/build/int_stack_tests[1]_include.cmake")
+subdirs("_deps/googletest-build")
