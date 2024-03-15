6af1f2bd0a9d62182b8b9d34b8f93d1c05af16fc
OakTree-42 <DannyGaev@gmail.com>
2024-03-15 17:53:45 -0500

    Added support for operations

--- r_forth/main.c
+++ r_forth/main.c
@@ -7,23 +7,72 @@
 int main(int argc, char *argv[])
 {
     char *token = generateSpaceless(argv[1]);
-    const int capacity = 3;
+    const int capacity = 4;
     int_stack_t myStack;
-        
-    printf("%d\n",capacity);
 
     int_stack_init(&myStack, capacity);
 
     while (token != NULL)
     {
         TOKEN returnToken = parseTokens(token);
-        
+
         printf("CLASS: %s, TEXT: %s\n", resolveToString(returnToken.type_t), returnToken.text);
-        int value = *returnToken.text - '0';
-        int success = int_stack_push(&myStack, value);
-        if (!success)
+        const int textLength = strlen(returnToken.text);
+        if (returnToken.type_t == NUM)
         {
-            fprintf(stderr, "Stack overflow: %d\n", value);
+            int value = *returnToken.text - '0';
+            int success = int_stack_push(&myStack, value);
+            if (!success)
+            {
+                fprintf(stderr, "Stack overflow: %d\n", value);
+            }
+        }
+        else if (returnToken.type_t == ARITH_OP)
+        {
+            char *answer = returnToken.text;
+            switch (*answer)
+            {
+            case '+':
+                printf("Adding top two elements...\n");
+                int_stack_add(&myStack);
+                break;
+            case '-':
+                printf("Subtracting top two elements...\n");
+                break;
+            case '*':
+                printf("Multiplying top two elements...\n");
+                break;
+            case '/':
+                printf("Deleting top two elements...\n");
+                break;
+            }
+        }
+        else if (returnToken.type_t == WORD)
+        {
+            char text[textLength];
+            strncpy(text, returnToken.text, textLength);
+            if (strcmp(text, "rot") == 0)
+            {
+                printf("Rotating\n");
+                int_stack_rot(&myStack);
+            }
+            else if (strcmp(text, "dup") == 0)
+            {
+                printf("Duplicating\n");
+                int_stack_dup(&myStack);
+            }
+            else if (strcmp(text, "swap") == 0)
+            {
+                int_stack_swap(&myStack);
+            }
+            else if (strcmp(text, "drop") == 0)
+            {
+                int_stack_drop(&myStack);
+            }
+            else if (strcmp(text, "over") == 0)
+            {
+                int_stack_over(&myStack);
+            }
         }
         token = strtok(NULL, " ");
     }
@@ -42,20 +91,20 @@ int main(int argc, char *argv[])
         }
     }
 
-    int_stack_print(&myStack, stdout);
+    // int_stack_print(&myStack, stdout);
 
-    int_stack_push(&myStack, 1);
-    int_stack_push(&myStack, 2);
-    int_stack_push(&myStack, 3);
-    int_stack_print(&myStack, stdout);
-    int_stack_swap(&myStack);
-    int_stack_print(&myStack, stdout);
-    int_stack_add(&myStack);
-    int_stack_print(&myStack, stdout); 
-    int_stack_dup(&myStack);
-    int_stack_print(&myStack, stdout); 
-    int_stack_add(&myStack);
-    int_stack_print(&myStack, stdout); 
-    
+    // int_stack_push(&myStack, 1);
+    // int_stack_push(&myStack, 2);
+    // int_stack_push(&myStack, 3);
+    // int_stack_print(&myStack, stdout);
+    // int_stack_swap(&myStack);
+    // int_stack_print(&myStack, stdout);
+    // int_stack_add(&myStack);
+    // int_stack_print(&myStack, stdout);
+    // int_stack_dup(&myStack);
+    // int_stack_print(&myStack, stdout);
+    // int_stack_add(&myStack);
+    // int_stack_print(&myStack, stdout);
+
     return EXIT_SUCCESS;
 }
