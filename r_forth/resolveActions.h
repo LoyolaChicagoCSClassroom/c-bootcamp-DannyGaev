9fbefde43006537cd857e4a04fd58d534a9d399c
OakTree-42 <DannyGaev@gmail.com>
2024-03-15 23:09:25 -0500

    Added necessary headings to resolveActions.h

--- r_forth/resolveActions.h
+++ r_forth/resolveActions.h
@@ -5,4 +5,8 @@ int_stack_t resolveArith(char *answer, int_stack_t myStack);
 
 int_stack_t resolveWord(char *answer, int_stack_t myStack, int textLength);
 
+int_stack_t resolveSymbol(char *answer, int_stack_t myStack);
+
+char *resolveArithOp(char *symbol);
+
 #endif
