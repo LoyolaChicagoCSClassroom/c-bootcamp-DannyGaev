b93de0d2b72faa2d954472dcc7d4cbb0d3164182
Daniel Gaevskiy <119715495+DannyGaev@users.noreply.github.com>
2024-03-16 01:19:14 -0500

    Delete r_forth/build directory

--- r_forth/build/int_stack_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/Users/oaktree/Documents/GitHub/c-bootcamp-DannyGaev/std-queue/build/int_stack_tests[1]_tests.cmake")
-  include("/Users/oaktree/Documents/GitHub/c-bootcamp-DannyGaev/std-queue/build/int_stack_tests[1]_tests.cmake")
-else()
-  add_test(int_stack_tests_NOT_BUILT int_stack_tests_NOT_BUILT)
-endif()
