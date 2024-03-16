195bd8de35be0c8352af52f49dafd245970e5b36
OakTree-42 <DannyGaev@gmail.com>
2024-03-15 22:09:40 -0500

    Added headers for resolveActions.c

---
+++ r_forth/resolveActions.h
@@ -0,0 +1,8 @@
+#ifndef RESOLVEACTIONS_H
+#define RESOLVEACTIONS_H
+
+int_stack_t resolveArith(char *answer, int_stack_t myStack);
+
+int_stack_t resolveWord(char *answer, int_stack_t myStack, int textLength);
+
+#endif
