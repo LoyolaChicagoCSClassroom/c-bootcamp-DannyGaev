f67a8e6e8229bc5d63b7f536bf07bba77536f485
OakTree-42 <DannyGaev@gmail.com>
2024-03-17 00:35:12 -0500

    Added headers for resolving variables

--- r_forth/resolveActions.h
+++ r_forth/resolveActions.h
@@ -1,10 +1,12 @@
 #ifndef RESOLVEACTIONS_H
 #define RESOLVEACTIONS_H
 
-int_stack_t resolveArith(char *answer, int_stack_t myStack);
+int_stack_t resolveArith(char *answer, int_stack_t myIntStack);
 
-int_stack_t resolveWord(char *answer, int_stack_t myStack, int textLength);
+int_stack_t resolveWord(char *answer, int_stack_t myIntStack, int textLength);
 
-int_stack_t resolveSymbol(char *answer, int_stack_t myStack);
+int_stack_t resolveSymbol(char *answer, int_stack_t myIntStack);
+
+general_stack_t resolveVariable(char *answer, general_stack_t mgs, int textLength);
 
 #endif
