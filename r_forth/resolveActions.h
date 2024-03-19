bdcfbc1a78836a813eb2d6174f57a7e1b55e3f28
OakTree-42 <DannyGaev@gmail.com>
2024-03-19 00:28:08 -0500

    Updated headers to account for new pointer arguments and void return values

--- r_forth/resolveActions.h
+++ r_forth/resolveActions.h
@@ -1,12 +1,13 @@
 #ifndef RESOLVEACTIONS_H
 #define RESOLVEACTIONS_H
 
-int_stack_t resolveArith(char *answer, int_stack_t myIntStack);
+// int_stack_t resolveArith(char *answer, int_stack_t myIntStack);
+void resolveArith(char *answer, int_stack_t *myIntStack);
 
-int_stack_t resolveWord(char *answer, int_stack_t myIntStack, int textLength);
+void resolveWord(char *answer, int_stack_t *myIntStack, int textLength);
 
-int_stack_t resolveSymbol(char *answer, int_stack_t myIntStack);
+void resolveSymbol(char *answer, int_stack_t *myIntStack);
 
-general_stack_t resolveVariable(char *answer, general_stack_t mgs, int textLength);
+void resolveVariable(char *answer, general_stack_t *myGeneralStack, int textLength);
 
 #endif
